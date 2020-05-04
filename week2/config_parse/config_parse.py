d63c55c72967dd2a84373835d30e2a2e7fe5579f
RuzannaO <63346028+RuzannaO@users.noreply.github.com>
2020-05-05 00:06:28 +0400

    Update config_parse.py

--- week2/config_parse/config_parse.py
+++ week2/config_parse/config_parse.py
@@ -1,17 +1,28 @@
 from collections import defaultdict
 
+def parser_ini(path):
 
-def nonempty(a):
-    return a
+    # function creates a dictionary from a section content line
+    def newdict(a):
+        newdict = {}
+        aaa = str(a).split(",")
+        for i in range(0, len(aaa)):
+            if aaa[i][aaa[i].index("=") + 1:].isnumeric():
+                newdict[aaa[i][:aaa[i].index("=")]] = int(aaa[i][aaa[i].index("=") + 1:])
+            else:
+                newdict[aaa[i][:aaa[i].index("=")]] = aaa[i][aaa[i].index("=") + 1:]
+        return newdict
 
 
-def parse_ini(path):
+    def nonempty(a):
+        return a
+
     # reads txt file data, creates a list "txtlines' comprised of lines from the txt file
     with open(path) as rdr:
         txtlines = rdr.read().splitlines()
-    # clear empty elements, i.e. empty lines
+    # removes empty elements, i.e. empty lines
     lines = list(filter(nonempty, txtlines))
-    # defines indexes which correspond to the sections and which to the content data (lists - sections_index and content_index)
+    # defines indexes which correspond to the sections and which to the content data (resulting lists - sections_index and content_index)
     sections_index = []
     content_index = []
     for i in range(0, len(lines)):
@@ -40,24 +51,33 @@ def parse_ini(path):
     if s > 0 or counter > 0:
         return ""
 
-    # defines the final dictionary
-    dict2 = defaultdict(set, {k:[] for k in sections_index})
+    # defines the result dictionary structure
 
     dict1 = defaultdict(set)
     for k in range(0, len(sections_index)):
 
         for j in content_index:
             if k == len(sections_index) - 1 and j > sections_index[k]:
-                dict1[lines[sections_index[k]]].add(lines[j])
+                dict1[lines[sections_index[k]]].add(j)
 
             else:
                 if j > sections_index[k] and j < sections_index[k + 1]:
-                    dict1[lines[sections_index[k]]].add(lines[j])
+                    dict1[lines[sections_index[k]]].add(j)
                 else:
                     dict1[lines[sections_index[k]]].add("")
                     dict1[lines[sections_index[k]]].remove("")
 
-    return (dict(dict1))
+    # the resulting dictionary
+    dict2 = defaultdict(dict, {k:{} for k in dict1.keys()})
+    for k in dict1.keys():
+        for j in dict1[k]:
+            dict2[k][list(newdict(lines[j]))[0]] = newdict(lines[j])[str(list(newdict(lines[j]))[0])]
+
+    return dict(dict2)
 
 
-print(parse_ini("test.ini"))
+
+print(parser_ini("test.ini"))
+
+
+
